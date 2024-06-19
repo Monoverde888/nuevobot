fd977dcae4357610c1817db6eabf8fff72c10d20
José Ojeda <minecraftersov@gmail.com>
2024-06-19 00:22:27 -0400

    ESTA VIVAAAAAAA

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { Client, Events, GatewayIntentBits } = require('discord.js');
 require('dotenv').config()
 
 
