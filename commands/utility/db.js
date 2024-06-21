2cbfb289cd8ed52f4fb7cb3d52dd3d12bb37f4f4
José Ojeda <minecraftersov@gmail.com>
2024-06-20 21:47:07 -0400

    OMG LA DB FUNCIONA?? PROBARE

--- commands/utility/db.js
+++ commands/utility/db.js
@@ -1,20 +1,27 @@
 const Keyv = require('keyv');
 const { SlashCommandBuilder } = require('discord.js');
-const keyv = new Keyv('sqlite://./db.sqlite');
+const keyv = new Keyv("sqlite:///home/mono/bot/db.sqlite");
 keyv.on('error', err => console.error('Keyv connection error:', err));
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('db')
 		.setDescription('USA LA INCREIBLE BASE DE DATOS SQLITE GRACIAS A A KEYV')
 		.addStringOption(option =>
+			option.setName('titulo')
+				.setDescription('TITULO DE LA COSA QUE VOY A AÑDIR A LA DB SUPER INCREIBLE')
+				.setRequired(true))
+		.addStringOption(option =>
 			option.setName('añadir')
 				.setDescription('QUE COSA AÑADIR A LA DB SUPER INCREIBLE')
 				.setRequired(true)),
 	async execute(interaction) {
+		const dbTitle = interaction.options.getString('titulo', true)
 		const dbAdd = interaction.options.getString('añadir', true)
 		await interaction.reply("OK VOY A AÑADIR" + dbAdd + "A LA BASE DE DATOS").then(() => {
-			console.log("APAGANDO POR ORDEN DE " + interaction.user.tag)
-			interaction.client.destroy();
+			keyv.set(dbTitle, dbAdd).then(() => {
+				interaction.editReply("AÑADIDO A LA BASE DE DATOS COMO" + keyv.get(dbTitle))
+			});
 		});
 	},
 }
+
