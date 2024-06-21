4ad3a1664bfd47efd72893d95b0f4ebb7e2edb8a
José Ojeda <minecraftersov@gmail.com>
2024-06-20 22:02:51 -0400

    eval mejorado 100%

--- events/handleModals.js
+++ events/handleModals.js
@@ -31,13 +31,11 @@ module.exports = {
 
 
 				await interaction.reply({
-					embeds: [new EmbedBuilder().setTitle('Evaluated Code').setDescription(`\`\`\`${escapeCodeBlock(result)}\`\`\``).setTimestamp()],
-					ephemeral: false,
+					embeds: [new EmbedBuilder().setTitle('Evaluated Code').setDescription(`\`\`\`js\n${escapeCodeBlock(result)}\`\`\``).setTimestamp()]
 				});
 
 				interaction.followUp({
-					files: [new AttachmentBuilder(Buffer.from(result, 'utf-8')).setName('Evaluated Code.txt')],
-					ephemeral: false,
+					files: [new AttachmentBuilder(Buffer.from(result, 'utf-8')).setName('Evaluated Code.txt')]
 				}); }
 			}
 
